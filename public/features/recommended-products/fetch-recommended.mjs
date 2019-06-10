65ee2ec5f9e5d678936b02e9417400b1f4bc351b
Joshua Harris <joshua.jharris13@gmail.com>
2019-06-09 20:02:21 -0400

    updates comment.

--- public/features/recommended-products/fetch-recommended.mjs
+++ public/features/recommended-products/fetch-recommended.mjs
@@ -1,5 +1,6 @@
 export default () => {
   const baseUrl = 'https://api.unsplash.com/search/photos';
+  // shhhhh, don't tell anyone :)
   const params = 'page=1&query=toy&client_id=ff5d1a05e1787f3a985f778af3d138c52bab071c5b2f2f73ba11721afcb8bd36';
 
   return fetch(`${baseUrl}?${params}`).then(
